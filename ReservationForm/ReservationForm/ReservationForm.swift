e7f528d89ec895db03bd7b5b39c1e257de6283aa
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-01-25 16:40:42 +0300

    Add files via upload

---
+++ ReservationForm/ReservationForm/ReservationForm.swift
@@ -0,0 +1,38 @@
+//
+//  ReservationForm.swift
+//  ReservationForm
+//
+//  Created by Emre Ülkem on 25.01.2023.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    @State var customerName: String = ""
+    var body: some View {
+        
+        Form {
+            TextField("Type Your Name",
+                  text: $customerName,
+                  onEditingChanged: { status in
+                    print(status)
+            })
+        
+            .onSubmit({
+            print("submitted.")
+            })
+        
+            .onChange(of: customerName, perform: { newValue in
+                print(newValue)
+            })
+        
+        }
+        .padding()
+    }
+}
+
+struct ReservationForm_Previews: PreviewProvider {
+    static var previews: some View {
+        ReservationForm()
+    }
+}
---
+++ ReservationForm/ReservationForm/ReservationFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationFormApp.swift
+//  ReservationForm
+//
+//  Created by Emre Ülkem on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReservationFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
