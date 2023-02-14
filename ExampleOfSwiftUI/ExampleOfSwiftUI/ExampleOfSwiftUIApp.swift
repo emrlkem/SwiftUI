7b18d284b8054645463be7323e69f2261f97d56f
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-02-14 14:25:32 +0100

    Add files via upload

---
+++ ExampleOfSwiftUI/ExampleOfSwiftUI/ExampleOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleOfSwiftUIApp.swift
+//  ExampleOfSwiftUI
+//
+//  Created by Emre Ülkem on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
